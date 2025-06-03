5e3a1311945dbe76d35f4fe6e2cf86ea0fcdce25
Fuji <fujiji.entire1414@gmail.com>
2025-06-03 17:04:54 +0900

    単位変換

--- a.cpp
+++ a.cpp
@@ -1,11 +1,12 @@
-#include <cmath>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  int a;
-  cin >> a;
-  int s = pow(a, 3);
-  cout << s << endl;
+  int s, h, m, se;
+  cin >> s;
+  h = s / 3600;
+  m = s % 3600 / 60;
+  se = s % 60;
+  cout << h << ":" << m << ":" << se << endl;
 }
