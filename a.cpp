e04a16f7a81d760d30034fd360b10646fdb4a93c
Fuji <fujiji.entire1414@gmail.com>
2025-06-04 16:13:40 +0900

    302_A

--- a.cpp
+++ a.cpp
@@ -1,24 +1,10 @@
+#include <cmath>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-  int n, a, b;
-  cin >> n >> a >> b;
-
-  vector<int> c(n);
-  for (int i = 0; i < n; i++) {
-    cin >> c[i];
-  }
-
-  int ans = a + b;
-  for (int i = 0; i < n; i++) {
-    if (ans == c[i]) {
-      cout << i + 1 << endl;
-      return 0;
-    }
-  }
-
-  return 0;
+  long long a, b;
+  cin >> a >> b;
+  cout << (a + b - 1) / b << endl;
 }
