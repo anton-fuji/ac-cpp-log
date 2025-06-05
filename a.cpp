0503fbb7b191ac15b34d015fba0f20cbd9bd4687
Fuji <fujiji.entire1414@gmail.com>
2025-06-05 15:57:57 +0900

    303_A

--- a.cpp
+++ a.cpp
@@ -1,10 +1,35 @@
-#include <cmath>
 #include <iostream>
+#include <string>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
 
+bool similar(char a, char b) {
+  if (a == b)
+    return true;
+  if (a == '1' && b == 'l')
+    return true;
+  if (a == 'l' && b == '1')
+    return true;
+  if (a == 'o' && b == '0')
+    return true;
+  if (a == '0' && b == 'o')
+    return true;
+  return false;
+}
+
 int main() {
-  long long a, b;
-  cin >> a >> b;
-  cout << (a + b - 1) / b << endl;
+  int n;
+  cin >> n;
+  string s, t;
+  cin >> s >> t;
+
+  rep(i, n) {
+    if (!similar(s[i], t[i])) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
 }
