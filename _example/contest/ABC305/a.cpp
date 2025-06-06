2bf98215760cd0841f13011939ae7ef913309153
Fuji <fujiji.entire1414@gmail.com>
2025-06-06 13:39:13 +0900

    abc305_A

--- _example/contest/ABC305/a.cpp
+++ _example/contest/ABC305/a.cpp
@@ -1,6 +1,13 @@
+#include <cmath>
 #include <iostream>
 #define rep(i, n) for (int i = 0; i < (n); ++ie
 
 using namespace std;
 
-int main() { cout << "Hello" << endl; }
+int main() {
+  int n;
+  cin >> n;
+
+  int ans = round(n / 5.0) * 5;
+  cout << ans << endl;
+}
