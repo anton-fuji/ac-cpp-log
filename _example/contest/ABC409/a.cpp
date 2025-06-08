2cf96537e3293bc74f4c97acb5534653b1d3f419
Fuji <fujiji.entire1414@gmail.com>
2025-06-08 21:28:22 +0900

    abc409_A

---
+++ _example/contest/ABC409/a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  string t, a;
+  cin >> t >> a;
+
+  int cnt = 0;
+  rep(i, n) {
+    if (t[i] == 'o' && a[i] == 'o') {
+      cnt++;
+    }
+  }
+  if (cnt > 0)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}
