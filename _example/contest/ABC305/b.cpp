30bf996e9cc449f2cdbb0001ab8511fcf6183da3
Fuji <fujiji.entire1414@gmail.com>
2025-06-09 15:22:52 +0900

    Modify ABC305_B

--- _example/contest/ABC305/b.cpp
+++ _example/contest/ABC305/b.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
+using ull = unsigned long long;
 
+int main() {
+  ull ans = 0;
+  rep(i, 64) {
+    ull a;
+    cin >> a;
+    ans += a * (1ull << i);
+  }
+  cout << ans << endl;
+  return 0;
+}
 int main() { cout << "Hello" << endl; }
