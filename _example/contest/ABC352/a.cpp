db8e015abe652d9887760d4f913cbe87fb9e1432
Fuji <fujiji.entire1414@gmail.com>
2025-06-11 06:22:03 +0900

    ABC352_A swapで場合分けを減らす

--- _example/contest/ABC352/a.cpp
+++ _example/contest/ABC352/a.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
@@ -7,16 +8,13 @@ int main() {
   int n, x, y, z;
   cin >> n >> x >> y >> z;
 
-  if (x < y) {
-    if (x < z && z < y)
-      cout << "Yes" << endl;
-    else
-      cout << "No" << endl;
-  } else {
-    if (x > z && z > y)
-      cout << "Yes" << endl;
-    else
-      cout << "No" << endl;
-  }
+  if (x > y)
+    swap(x, y);
+
+  if (x < z && z < y)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
   return 0;
 }
