b31c4961ad83b4e1b9f7c1160d58cf3cb6b934ee
Fuji <fujiji.entire1414@gmail.com>
2025-06-21 17:13:16 +0900

    abc351_A 復習

--- _practice/a.cpp
+++ _practice/a.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <vector>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
+using ll = long long;
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  vector<ll> a(9), b(8);
+  rep(i, 9) cin >> a[i];
+  rep(i, 8) cin >> b[i];
 
-  if (a == b) {
-    cout << -1 << endl;
-    return 0;
-  }
+  int t_sum = 0;
+  rep(i, 9) { t_sum += a[i]; }
 
-  int ans = a + b;
-  cout << (6 - ans) << endl;
+  int a_sum = 0;
+  rep(i, 8) { a_sum += b[i]; }
+
+  int ans = t_sum - a_sum + 1;
+  cout << ans << endl;
+
   return 0;
 }
