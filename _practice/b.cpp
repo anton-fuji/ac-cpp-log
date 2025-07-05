b6c38e1b43bd048828368558882bd410594f19d9
Fuji <fujiji.entire1414@gmail.com>
2025-07-05 16:15:46 +0900

    ABC_412のB復習

--- _practice/b.cpp
+++ _practice/b.cpp
@@ -1,27 +1,22 @@
-#include <algorithm>
+#include <cctype>
 #include <iostream>
-#include <utility>
-#include <vector>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
 
 int main() {
-  int n, m;
-  cin >> n >> m;
-  vector<int> a(n), b(m);
-  rep(i, n) cin >> a[i];
-  rep(i, m) cin >> b[i];
-  vector<pair<int, int>> p;
-  rep(i, n) p.emplace_back(a[i], 0);
-  rep(i, m) p.emplace_back(b[i], 1);
-  sort(p.begin(), p.end());
-  rep(i, n + m - 1) {
-    if (p[i].second == 0 && p[i + 1].second == 0) {
-      cout << "Yes" << endl;
-      return 0;
+  string s, t;
+  cin >> s >> t;
+
+  bool res = true;
+  int n = s.length();
+  for (int i = 1; i < n; i++) {
+    if (isupper(s[i])) {
+      if (t.find(s[i - 1]) == string::npos) {
+        res = false;
+      }
     }
   }
-  cout << "No" << endl;
+  cout << (res ? "Yes" : "No") << endl;
   return 0;
 }
